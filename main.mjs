81c04ea5a663ec9fc6ba5ce39faf6c0576d4389d
Muhammad Ahad <120649081+ahadsts9901@users.noreply.github.com>
2023-06-17 11:33:29 -0700

    name added

--- main.mjs
+++ main.mjs
@@ -3,23 +3,23 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World!' + new Date())
+    res.send('Hello World! Muhammad Ahad' + new Date())
 })
 
 app.get('/profile', (req, res) => {
-    res.send('this is profile...!' + new Date())
+    res.send('this is profile...! Muhammad Ahad' + new Date())
 })
 
 app.get('/about', (req, res) => {
-    res.send('this is about...!' + new Date())
+    res.send('this is about...! Muhammad Ahad' + new Date())
 })
 
 app.get('/contact', (req, res) => {
-    res.send('this is contact...!' + new Date())
+    res.send('this is contact...! Muhammad Ahad' + new Date())
 })
 
 app.get('/gallery', (req, res) => {
-    res.send('this is gallery...!' + new Date())
+    res.send('this is gallery...! Muhammad Ahad' + new Date())
 })
 
 app.listen(PORT, () => {
